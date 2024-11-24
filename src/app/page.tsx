1cde85c908a81feccaf98d97de60514e62043ca9
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2024-11-24 14:34:22 +0100

    Update `Navbar` import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Navbar from "./Navbar";
+import { Navbar } from "./components/Navbar";
 
 export default function Home() {
-  return <div className="text-white h-full min-h-screen flex justify-center w-full bg-gradient-to-b from-[#030313] to-[#040A2E]">
-    <div className="container w-full p-6">
-      <Navbar />
+  return (
+    <div className="text-white h-full min-h-screen flex justify-center w-full bg-gradient-to-b from-[#030313] to-[#040A2E]">
+      <div className="container w-full p-6">
+        <Navbar />
+      </div>
     </div>
-  </div>;
+  );
 }
