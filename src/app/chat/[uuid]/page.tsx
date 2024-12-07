5d548dc165625816bf21d66fb2afd4a50642bb90
Wojciech Kosmalski <jawmen999@gmail.com>
2024-12-07 21:21:36 +0100

    fix: fix lint issues

--- src/app/chat/[uuid]/page.tsx
+++ src/app/chat/[uuid]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { usePathname } from "next/navigation";
+//import { usePathname } from "next/navigation";
 
 export default function ConversationPage() {
+  /*
   const pathname = usePathname(); // pathname = /chat/{uuid}
-  //TODO load chat from local storage using uuid, if not found -> 404
+  TODO load chat from local storage using uuid, if not found -> 404
+  */
 
   return <div></div>;
 }
