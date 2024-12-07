8ed9c45b038af0f9cba97e79e2ac014bb7fa26d1
Wojciech Kosmalski <jawmen999@gmail.com>
2024-12-07 21:11:18 +0100

    refact: remove code for testing; rename tailwind color variable

--- src/app/chat/[uuid]/page.tsx
+++ src/app/chat/[uuid]/page.tsx
@@ -2,9 +2,8 @@
 import { usePathname } from "next/navigation";
 
 export default function ConversationPage() {
-  const pathname = usePathname();
-  const uuidRegex =
-    /[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/g;
-  const uuid = pathname.match(uuidRegex)?.[0];
-  return <div>UUID: {uuid}</div>;
+  const pathname = usePathname(); // pathname = /chat/{uuid}
+  //TODO load chat from local storage using uuid, if not found -> 404
+
+  return <div></div>;
 }
