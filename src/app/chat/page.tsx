9a26830b01305e1053b39e6f3f08f620728ce288
Maciej Malinowski <mejsiejdev@gmail.com>
2024-12-08 20:18:34 +0100

    feat(supervisor): Add highlight state

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,14 +3,26 @@ import { Supervisor } from "@/components/Supervisor";
 
 export default function ChatPage() {
   return (
-    <div className="flex flex-col gap-4 flex-grow items-center justify-center">
+    <div className="flex flex-grow flex-col items-center justify-center gap-4">
       <Chat />
       <div className="space-y-4">
         <div className="flex items-center gap-4">
-          <PromochatorIcon imageWidth={36} imageHeight={36} imageClassName="py-2 px-1" />
-          <p className="py-3 px-4 bg-chat-bot rounded-2xl">Oto promotorzy, którzy mogą Ci pomóc w realizacji tego tematu:</p>
+          <PromochatorIcon
+            imageWidth={36}
+            imageHeight={36}
+            imageClassName="py-2 px-1"
+          />
+          <p className="rounded-2xl bg-chat-bot px-4 py-3">
+            Oto promotorzy, którzy mogą Ci pomóc w realizacji tego tematu:
+          </p>
         </div>
-        <Supervisor />
+        <Supervisor
+          name="Dr hab. Anna Nowak"
+          specialization="Specjalistka w dziedzinie UX Design i Psychologii"
+          description="Współpracowała z wieloma firmami IT, pomagając im tworzyć produkty
+          przyjazne dla użytkownika."
+          highlight
+        />
       </div>
     </div>
   );
