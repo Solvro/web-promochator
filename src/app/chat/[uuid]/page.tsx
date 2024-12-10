47ecb1e0b13037b84a1b3594a2da54b130967d5d
Maciej Malinowski <mejsiejdev@gmail.com>
2024-12-10 12:02:25 +0100

    feat(chatpage): Throw page not found error if chat is not in localStorage

--- src/app/chat/[uuid]/page.tsx
+++ src/app/chat/[uuid]/page.tsx
@@ -1,11 +1,20 @@
 "use client";
-//import { usePathname } from "next/navigation";
 
-export default function ConversationPage() {
+import { notFound, useParams } from "next/navigation";
+
+export default function ConversationPage({}) {
   /*
   const pathname = usePathname(); // pathname = /chat/{uuid}
   TODO load chat from local storage using uuid, if not found -> 404
   */
 
-  return <div></div>;
+  const params = useParams<{ uuid: string }>();
+
+  const chat = localStorage.getItem(params.uuid);
+
+  if (!chat) {
+    notFound();
+  }
+
+  return <div>{params.uuid}</div>;
 }
