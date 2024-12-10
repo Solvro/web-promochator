28ee3bafafe8475d96af81fa90208152b3499e34
Maciej Malinowski <mejsiejdev@gmail.com>
2024-12-10 12:37:54 +0100

    refactor(chatpage): remove comment

--- src/app/chat/[uuid]/page.tsx
+++ src/app/chat/[uuid]/page.tsx
@@ -3,11 +3,6 @@
 import { notFound, useParams } from "next/navigation";
 
 export default function ConversationPage({}) {
-  /*
-  const pathname = usePathname(); // pathname = /chat/{uuid}
-  TODO load chat from local storage using uuid, if not found -> 404
-  */
-
   const params = useParams<{ uuid: string }>();
 
   const chat = localStorage.getItem(params.uuid);
