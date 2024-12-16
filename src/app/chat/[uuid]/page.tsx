f5047ba95984a48c9ca5def9becd762bac27e86d
Wojciech Kosmalski <jawmen999@gmail.com>
2024-12-16 23:49:46 +0100

    fix: wrap accessing localstorage with useEffect

--- src/app/chat/[uuid]/page.tsx
+++ src/app/chat/[uuid]/page.tsx
@@ -1,24 +1,32 @@
 "use client";
 
 import { notFound, useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import type { Chat } from "@/types/chat";
 
 export default function ConversationPage() {
   const parameters = useParams<{ uuid: string }>();
 
-  // Get chats from local storage
-  const chats =
-    localStorage.getItem("chats") == null
-      ? []
-      : (JSON.parse(localStorage.getItem("chats")!) as Chat[]);
+  const [chat, setChat] = useState<Chat | null>(null);
 
-  // Find chat by uuid
-  const chat = chats.find((c) => c.uuid === parameters.uuid);
+  useEffect(() => {
+    const chats =
+      localStorage.getItem("chats") == null
+        ? []
+        : (JSON.parse(localStorage.getItem("chats")!) as Chat[]);
 
-  // If chat not found, return 404
+    const foundChat = chats.find((c) => c.uuid === parameters.uuid);
+
+    if (foundChat) {
+      setChat(foundChat);
+    } else {
+      notFound();
+    }
+  }, [parameters.uuid]);
+
   if (!chat) {
-    notFound();
+    return <div>Loading...</div>;
   }
 
   return <div>{chat.prompt}</div>;
