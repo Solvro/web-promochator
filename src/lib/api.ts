334228ab0e5fd8f21b8bcfed1c632d4438bbf98b
Wojciech Kosmalski <jawmen999@gmail.com>
2024-12-20 20:42:00 +0100

    feat: add checking if env is set

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,13 @@
 export const API_URL = process.env.PROMOCHATOR_API ?? "";
 
-/**@param endpoint endpoint path **WITH** leading slash */
+if (API_URL === "") {
+  throw new Error("API_URL was not set in enviroment variables!");
+}
+
+/**
+ * Calling Promochator backend API
+ * @param endpoint endpoint path **WITH** leading slash
+ */
 export async function fetchData<T>(endpoint: string, options?: RequestInit) {
   const response = await fetch(`${API_URL}${endpoint}`, {
     ...options,
