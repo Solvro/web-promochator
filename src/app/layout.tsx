5c0a15ff2f54323c4ea86c573f31373573fc68ac
Maciej Król <maciekgplay@gmail.com>
2024-12-21 20:12:59 +0100

    chore: add basic metadata fields

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,32 @@ import { Providers } from "@/components/providers";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://promochator.solvro.pl"),
   title: "PromoCHATor",
-  description: "Znajdź swojego promotora!",
+  description:
+    "Nasz inteligentny system przeanalizuje temat Twojej pracy dyplomowej i przedstawi Ci najbardziej odpowiednich promotorów z kadry akademickiej PWr wraz z ich powiązanymi pracami",
+  keywords: [
+    "praca dyplomowa",
+    "praca magisterska",
+    "praca inżynierska",
+    "promotorzy pwr",
+    "wyszukiwarka promotorów",
+    "dobór promotora",
+    "politechnika wrocławska",
+    "studia pwr",
+    "solvro",
+  ],
+  openGraph: {
+    title: "PromoCHATor",
+    description:
+      "Nasz inteligentny system przeanalizuje temat Twojej pracy dyplomowej i przedstawi Ci najbardziej odpowiednich promotorów z kadry akademickiej PWr wraz z ich powiązanymi pracami",
+    url: "https://promochator.solvro.pl",
+    siteName: "PromoCHATor",
+    type: "website",
+    locale: "pl_PL",
+    // TODO: Add image
+    // images: './opengraph-image.png',
+  },
 };
 
 const poppins = Poppins({
