31ae1aae039a481793fedfa37abdbde5d846d135
Wojciech Kosmalski <jawmen999@gmail.com>
2025-02-05 20:22:40 +0100

    feat: mock fetch when developing

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -4,8 +4,13 @@ import { fetchData } from "@/lib/api";
 import type { Feedback } from "@/types/api-types";
 
 export async function POST(request: Request) {
-  const body = (await request.json()) as Feedback;
   try {
+    const body = (await request.json()) as Feedback;
+
+    if (process.env.NODE_ENV === "development") {
+      return NextResponse.json(null, { status: 201 });
+    }
+
     await fetchData("/recommend/feedback", {
       body: JSON.stringify(body),
       method: "POST",
