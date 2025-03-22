ebf6d72190ac231b4319be3e7f3d2aa6f7368b23
Wojciech Kosmalski <jawmen999@gmail.com>
2025-03-22 18:49:20 +0100

    chore: remove dyzury app ad

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 
 import { ClientOnly } from "./client-only";
-import { DyzuryAppAd } from "./dyzury-app-ad";
 import { PromptForm } from "./prompt-form";
 
 export function Chat() {
@@ -11,7 +10,6 @@ export function Chat() {
       <div className="flex w-full flex-col items-center gap-y-3">
         <p className="text-lg">Pomóc wybrać Ci promotora?</p>
         <PromptForm />
-        <DyzuryAppAd />
       </div>
     </ClientOnly>
   );
